1b87698f8b3d6dcab07cd6103c6fb6d1ecf469bc
teteucria <matheusfernandes00989@gmail.com>
2026-01-06 05:25:08 -0300

    Update configuration values in settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@
 module.exports = {
   SESSION_ID: "",  // add session Id 
   
-  OWNER_NUMBER: "923427582273", // add owner number 
+  OWNER_NUMBER: "16981259752", // add owner number 
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: "!", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Asia/Karachi" // put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Asia" // put your country timeZone....leave blank if u don't know.
 };
 
